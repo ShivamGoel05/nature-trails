578b3512dd9c819c54d840017d5c87bf307d7a56
Shivam Goel <110183387+ShivamGoel05@users.noreply.github.com>
2026-03-31 22:50:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -278,6 +278,8 @@ app.use((req, res) => {
     res.status(404).render('error');
 });
 
-app.listen(3000, () => {
-    console.log('Listening on Port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Listening on Port ${PORT}`);
 });
